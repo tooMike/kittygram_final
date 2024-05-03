b6f6b78c37c4c1bc4e583728add27eba64e71829
Михаил Беляев <belyaev-m-o@yandex.ru>
2024-05-03 17:11:58 +0200

    Final Version (review 1) v3

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,8 +95,8 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-# STATIC_ROOT = '/backend_static/static'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = '/backend_static/static'
+# STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
