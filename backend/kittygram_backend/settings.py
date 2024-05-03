5d055a03e4dfed39a7174b7bf22e13a3b0422268
Михаил Беляев <belyaev-m-o@yandex.ru>
2024-05-03 17:27:27 +0200

    Final Version (review 1) v4

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -95,9 +95,11 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
+
 STATIC_ROOT = '/backend_static/static'
 
 MEDIA_URL = '/media/'
+
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
